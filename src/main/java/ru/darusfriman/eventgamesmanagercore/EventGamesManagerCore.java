bbdaab8019a0040d9e9cc5586c5cbf84252d784c
Dark-Fallen <57632841+Dark-Fallen@users.noreply.github.com>
2025-08-13 09:40:13 +0300

    Create EventGamesManagerCore.java

---
+++ src/main/java/ru/darusfriman/eventgamesmanagercore/EventGamesManagerCore.java
@@ -0,0 +1,14 @@
+package ru.darusfriman.eventgamesmanagercore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EventGamesManagerCore extends JavaPlugin {
+    public EventGamesManagerCore() {
+    }
+
+    public void onEnable() {
+    }
+
+    public void onDisable() {
+    }
+}

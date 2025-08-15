4748d98dacdecd085a0fcc6619e2b18b87d9536f
Dark-Fallen <57632841+Dark-Fallen@users.noreply.github.com>
2025-08-15 21:07:31 +0300

    Add files via upload

---
+++ src/main/java/ru/darusfriman/eventgamesmanagercore/Game/WorldsManager.java
@@ -0,0 +1,20 @@
+package ru.darusfriman.eventgamesmanagercore.Game;
+
+public class WorldsManager {
+
+    public static void CopyWorld(String worldName, String copyWorldName)
+    {
+
+    }
+
+    public static void CopyWorld(String worldName)
+    {
+
+    }
+
+    public static void CreateWorld(String worldName)
+    {
+
+    }
+
+}
